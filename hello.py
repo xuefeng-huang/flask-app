c6c5ae190f181e7c8ca9c633a7e86c14c1a8b1f7
xuefeng_huang <timhxf@gmail.com>
2015-12-16 04:40:43 +0000

    initial commit flask app

---
+++ hello.py
@@ -0,0 +1,14 @@
+import os
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    """docstring for hello_world"""
+    return 'Hello World!'
+    
+if __name__ == '__main__':
+    host = os.getenv('IP', '0.0.0.0')
+    port = int(os.getenv('PORT', 5000))
+    app.run(host = host, port = port)
